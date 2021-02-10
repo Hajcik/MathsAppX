b2ffbc53966cb157c65eafb7ce2ade2f122c64eb
Hajcik <hajcik666pl@gmail.com>
2021-02-10 15:48:51 +0100

    Initial commit

---
+++ MathsAppX/MathsAppX/MathsAppX/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MathsAppX.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MathsAppX.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MathsAppX/MathsAppX/MathsAppX/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MathsAppX.Models;
+using MathsAppX.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MathsAppX.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
